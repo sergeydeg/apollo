b5008332f201432ef83faa159554e9042318392c
James Gayfer <gayfer.james@gmail.com>
2018-06-10 21:36:59 -0700

    Add custom command prefix to Guild

--- event_bot/models/guild.py
+++ event_bot/models/guild.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import Column, Integer, Text, ForeignKey
 from sqlalchemy.orm import relationship
 
 from . import Base
@@ -6,5 +6,6 @@ from . import Base
 class Guild(Base):
     __tablename__ = 'guild'
     id = Column(Integer, primary_key=True)
+    prefix = Column(Text)
 
     event_channels = relationship("EventChannel", passive_deletes=True)
