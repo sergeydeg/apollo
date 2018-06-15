fdfe6a04af1a5dea0f6af94c71e0a4f420ffe82f
James Gayfer <gayfer.james@gmail.com>
2018-06-14 19:27:34 -0700

    Use proper UTF-8 encoding in database
    MySQL does not properly implement UTF-8. This change allows for true
    UTF-8, which is needed for storing things like emojis.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db_user = os.getenv('DB_USER', 'root')
 db_pass = os.getenv('DB_PASS', '')
 db_name = os.getenv('DB_NAME', 'event_bot')
 
-engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/{db_name}')
+engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/{db_name}?charset=utf8mb4')
 Session = sessionmaker()
 Session.configure(bind=engine)
 
