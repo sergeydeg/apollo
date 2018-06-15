8380b0758b0030078069c06d60d166706ce8101f
James Gayfer <gayfer.james@gmail.com>
2018-06-14 21:35:30 -0700

    Fix database encoding in console.py
    When the encoding was changed, it should have been changed here as well.

--- bin/console.py
+++ bin/console.py
@@ -10,8 +10,9 @@ from event_bot.models import *
 
 db_user = os.getenv('DB_USER', 'root')
 db_pass = os.getenv('DB_PASS', '')
+db_name = os.getenv('DB_NAME', 'event_bot')
 
-engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/event_bot')
+engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/{db_name}?charset=utf8mb4')
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
