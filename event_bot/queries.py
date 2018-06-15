2a1f1e64d41aac6feff1871050c8579a1e707cdb
James Gayfer <gayfer.james@gmail.com>
2018-06-15 08:57:39 -0700

    Update queries.py to use Transaction

--- event_bot/queries.py
+++ event_bot/queries.py
@@ -1,19 +1,19 @@
 from .models import Guild, User
 
 
-def find_or_create_user(session_scope, id):
+def find_or_create_user(transaction, id):
     """Find the user with the given id. If one doesn't exist, create it."""
-    return find_or_create_model(session_scope, User, id) 
+    return find_or_create_model(transaction, User, id) 
 
 
-def find_or_create_guild(session_scope, id):
+def find_or_create_guild(transaction, id):
     """Find the guild with the given id. If it doesn't exist, create it."""
-    return find_or_create_model(session_scope, Guild, id) 
+    return find_or_create_model(transaction, Guild, id) 
 
 
-def find_or_create_model(session_scope, Model, id):
+def find_or_create_model(transaction, Model, id):
     """Find the given model with the given id. If it doesn't exist, create it."""
-    with session_scope.new() as session:
+    with transaction.new() as session:
         model = session.query(Model).filter(Model.id == id).first()
         if not model:
             model = Model(id=id)
