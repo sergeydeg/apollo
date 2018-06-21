b2ee320df8cef38f626738482a2d628978178411
James Gayfer <gayfer.james@gmail.com>
2018-06-20 21:54:30 -0700

    Allow SQLAlchemy options to be passed to queries
    This allows us to do things like explicitly define lazy/eager loading,
    while still keeping common queries wrapped up in functions.

--- event_bot/queries.py
+++ event_bot/queries.py
@@ -1,20 +1,23 @@
 from .models import Guild, User
 
 
-def find_or_create_user(transaction, id):
+def find_or_create_user(transaction, id, options=None):
     """Find the user with the given id. If one doesn't exist, create it."""
-    return find_or_create_model(transaction, User, id) 
+    return find_or_create_model(transaction, User, id, options)
 
 
-def find_or_create_guild(transaction, id):
+def find_or_create_guild(transaction, id, options=None):
     """Find the guild with the given id. If it doesn't exist, create it."""
-    return find_or_create_model(transaction, Guild, id) 
+    return find_or_create_model(transaction, Guild, id, options)
 
 
-def find_or_create_model(transaction, Model, id):
+def find_or_create_model(transaction, Model, id, options=None):
     """Find the given model with the given id. If it doesn't exist, create it."""
     with transaction.new() as session:
-        model = session.query(Model).filter(Model.id == id).first()
+        if options:
+            model = session.query(Model).options(options).filter(Model.id == id).first()
+        else:
+            model = session.query(Model).filter(Model.id == id).first()
         if not model:
             model = Model(id=id)
             session.add(model)
