9aba4773f40d7f6f5eccad98b003647cff0d8b4c
James Gayfer <gayfer.james@gmail.com>
2018-06-20 20:42:00 -0700

    Setup logging

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -6,11 +7,17 @@ from event_bot import *
 from event_bot.commands import * 
 from event_bot.events import *
 
+logging.basicConfig(level=logging.INFO)
+
+env = os.getenv('ENV', 'develop')
 db_user = os.getenv('DB_USER', 'root')
 db_pass = os.getenv('DB_PASS', '')
 db_name = os.getenv('DB_NAME', 'event_bot')
 
 engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/{db_name}?charset=utf8mb4')
+if env == 'develop':
+    engine.echo = True
+
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
