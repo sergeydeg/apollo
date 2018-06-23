f11e91c7a6f8c98ca9c693bb4fc36fd94f475c33
James Gayfer <gayfer.james@gmail.com>
2018-06-23 11:38:29 -0700

    Change name of event organizer column
    The previous name of 'user_id' makes sense in relation to the table it
    refers to, but doesn't make a ton of sense in relation to an event. I've
    changed the name to 'organizer_id' to better communicate what relation
    the user has to the event.

--- event_bot/models/event.py
+++ event_bot/models/event.py
@@ -8,7 +8,7 @@ class Event(Base):
     __tablename__ = 'event'
     id = Column(Integer, primary_key=True)
     event_channel_id = Column(BigInteger, ForeignKey('event_channel.id', ondelete='CASCADE'))
-    user_id = Column(BigInteger, ForeignKey('user.id', ondelete='CASCADE'))
+    organizer_id = Column(BigInteger, ForeignKey('user.id', ondelete='CASCADE'))
     message_id = Column(BigInteger)
     title = Column(Text)
     description = Column(Text)
