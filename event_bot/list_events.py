976f8403637d19664bb326b4545c9fae5f1fa94f
James Gayfer <gayfer.james@gmail.com>
2018-07-06 19:48:36 -0700

    Code styles fixes
    Some of these lines were a little lengthy, which was hampering overall
    readability.

--- event_bot/list_events.py
+++ event_bot/list_events.py
@@ -1,14 +1,19 @@
 from .embeds import event_embed
 from . import emojis as emoji
 
+
 async def list_events(bot, event_channel):
-    """Clear the given event channel and then populate it with its events"""
+    """Clear the event channel and populate it with events"""
     channel = bot.get_channel(event_channel.id)
     await channel.purge()
 
     for event in event_channel.events:
-        organizer = bot.find_guild_member(event_channel.guild_id, event.organizer_id)
-        event_msg = await channel.send(embed=event_embed(event, organizer.display_name))
+        organizer = bot.find_guild_member(
+            event_channel.guild_id,
+            event.organizer_id
+        )
+        embed = event_embed(event, organizer.display_name)
+        event_msg = await channel.send(embed=embed)
         await _add_rsvp_reactions(event_msg)
         event.message_id = event_msg.id
 
