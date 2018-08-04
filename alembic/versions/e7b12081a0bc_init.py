2bbbd77e79b4856bf149b6452d9fa73871e8ad67
James Gayfer <gayfer.james@gmail.com>
2018-08-04 11:39:22 -0700

    Add initial migration

---
+++ alembic/versions/e7b12081a0bc_init.py
@@ -0,0 +1,24 @@
+"""Init
+
+Revision ID: e7b12081a0bc
+Revises: 
+Create Date: 2018-08-04 11:38:56.173423
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e7b12081a0bc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
