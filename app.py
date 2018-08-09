e1375bcbad64c7fb97ba8d527fc93da2d99e1b11
James Gayfer <gayfer.james@gmail.com>
2018-08-08 21:34:03 -0700

    Recycle stale database connections
    In the event that there is no interaction with the database for 8 hours,
    the connection will be closed (MySQL default). To prevent this, we will
    recycle connections that have not been used in the last hour.
    
    This won't be a problem once more people start using the bot, but
    currently it can cause the bot to be unresponsive so we should fix this.

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ db_user = os.getenv('DB_USER', 'root')
 db_pass = os.getenv('DB_PASS', '')
 db_name = os.getenv('DB_NAME', 'apollo')
 
-engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/{db_name}?charset=utf8mb4')
+engine = create_engine(f'mysql://{db_user}:{db_pass}@localhost/{db_name}?charset=utf8mb4', pool_recycle=3600)
 if env == 'develop':
     engine.echo = True
 
