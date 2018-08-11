689c77444ff54d2e306659d32823d81d243af9bf
James Gayfer <gayfer.james@gmail.com>
2018-08-10 22:53:49 -0700

    Add standard NA time zones

--- apollo/time_zones.py
+++ apollo/time_zones.py
@@ -1,3 +1,6 @@
 VALID_TIME_ZONES = {
-    "PST" : "US/Pacific"
+    "PST" : "America/Los_Angeles",
+    "MST" : "America/Denver",
+    "CST" : "America/Mexico_City",
+    "EST" : "America/Detroit"
 }
