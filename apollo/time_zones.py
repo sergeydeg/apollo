67af481978d914f60dabd1d67e08cba6f8ba7758
James Gayfer <gayfer.james@gmail.com>
2018-08-19 20:13:54 -0700

    Add AEST time zone

--- apollo/time_zones.py
+++ apollo/time_zones.py
@@ -2,5 +2,6 @@ VALID_TIME_ZONES = {
     "PST" : "America/Los_Angeles",
     "MST" : "America/Denver",
     "CST" : "America/Mexico_City",
-    "EST" : "America/Detroit"
+    "EST" : "America/Detroit",
+    "AEST" : "Australia/Sydney"
 }
