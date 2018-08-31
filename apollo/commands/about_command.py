c472087517d3c05b457a8825361465227e47045a
James Gayfer <gayfer.james@gmail.com>
2018-08-30 20:50:41 -0700

    Add about command help text

--- apollo/commands/about_command.py
+++ apollo/commands/about_command.py
@@ -11,6 +11,7 @@ class AboutCommand:
 
     @commands.command()
     async def about(self, ctx):
+        """Display information about the bot"""
         session = self.bot.Session()
         about_embed = AboutEmbed(self.bot, session).call()
         await ctx.send(embed=about_embed)
