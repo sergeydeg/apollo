ae827d59a5debc8614f77f664351613ac816ff87
James Gayfer <gayfer.james@gmail.com>
2018-12-01 16:30:46 -0800

    Prevent users from being locked out of events
    The code was set up in such a way that we could
    lock out users from RSVPing to events if there was
    an unexpected error while processing their reaction.
    
    I've changed things up so that we still do our clean
    up on errors.

--- apollo/events/on_raw_reaction_add.py
+++ apollo/events/on_raw_reaction_add.py
@@ -2,6 +2,7 @@ from apollo.models import Event
 from apollo.services import HandleEventReaction
 from apollo.queries import find_event_from_message, find_or_create_user
 
+
 class OnRawReactionAdd:
 
     def __init__(self, bot):
@@ -15,6 +16,7 @@ class OnRawReactionAdd:
             if payload.user_id == self.bot.user.id:
                 return
 
+            # Process event reaction
             if self.bot.cache.event_exists(payload.message_id):
 
                 # Stop if already procesing a reaction for this user
@@ -22,19 +24,19 @@ class OnRawReactionAdd:
                     await self.remove_reaction(payload)
                     return
 
-                self.users_reacting.append(payload.user_id)
-
-                event = find_event_from_message(session, payload.message_id)
-                find_or_create_user(session, payload.user_id)
-
-                await HandleEventReaction(
-                    self.bot,
-                    session,
-                    event,
-                    payload).call()
-
-                await self.remove_reaction(payload)
-                self.users_reacting.remove(payload.user_id)
+                try:
+                    self.users_reacting.append(payload.user_id)
+                    event = find_event_from_message(session, payload.message_id)
+                    find_or_create_user(session, payload.user_id)
+                    await HandleEventReaction(
+                        self.bot,
+                        session,
+                        event,
+                        payload).call()
+                finally:
+                    # Clean up to ensure we don't enter an error state
+                    await self.remove_reaction(payload)
+                    self.users_reacting.remove(payload.user_id)
 
 
     async def remove_reaction(self, payload):
