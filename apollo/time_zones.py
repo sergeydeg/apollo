a39a0fed6458ef39db24cd64154f6b63c3f5381e
James Gayfer <gayfer.james@gmail.com>
2019-06-02 11:05:02 -0700

    Remove unused time zone definitions

--- apollo/time_zones.py
+++ apollo/time_zones.py
@@ -1,17 +1,3 @@
-VALID_TIME_ZONES = {
-    "SST"  : "Asia/Singapore",
-    "PST"  : "America/Los_Angeles",
-    "MST"  : "America/Denver",
-    "CST"  : "America/Mexico_City",
-    "EST"  : "America/Detroit",
-    "AWST" : "Australia/Perth",
-    "AEST" : "Australia/Sydney",
-    "GMT"  : "Europe/London",
-    "BST"  : "Europe/London",
-    "CET"  : "Europe/Paris",
-    "CEST" : "Europe/Paris",
-}
-
 ISO_TIME_ZONES = [
     "America/Los_Angeles",
     "America/Denver",
