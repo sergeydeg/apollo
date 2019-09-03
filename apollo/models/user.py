3b3ed7e9371ec01f660aea2196ef4702578aba7a
tchan <tchanbc@gmail.com>
2019-09-02 19:39:07 -0700

    Change time_zone to 50

--- apollo/models/user.py
+++ apollo/models/user.py
@@ -7,5 +7,5 @@ from . import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(BigInteger, primary_key=True)
-    time_zone = Column(String(32))
+    time_zone = Column(String(50))
     events = relationship("Event", back_populates="organizer")
