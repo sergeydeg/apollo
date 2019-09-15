048c6dc8f8d1ca17d6dee61580bd0a92dcad27c2
James Gayfer <gayfer.james@gmail.com>
2019-09-15 10:54:03 -0700

    Split time zones by region
    It's getting to the point where it's not easy to parse the long list of
    time zones that Apollo supports.
    
    To make things easier, let's split up the list by region.

--- apollo/input/time_zone_input.py
+++ apollo/input/time_zone_input.py
@@ -13,15 +13,21 @@ class TimeZoneInput:
         :param channel: Messageable, e.g. context.author.dmchannel
         :return: str
         """
+        all_time_zones = []
+
+        for time_zones in ISO_TIME_ZONES.values():
+            for time_zone in time_zones:
+                all_time_zones.append(time_zone)
+
         while True:
             resp = (await self.bot.get_next_message(user, channel)).content
 
-            if not self._valid_time_zone_input(resp):
+            if not self._valid_time_zone_input(resp, len(all_time_zones)):
                 await channel.send(t("event.invalid_time_zone"))
                 continue
 
             time_zone_index = int(resp) - 1
-            return ISO_TIME_ZONES[time_zone_index]
+            return all_time_zones[time_zone_index]
 
-    def _valid_time_zone_input(self, value):
-        return value.isdigit() and int(value) in range(1, len(ISO_TIME_ZONES) + 1)
+    def _valid_time_zone_input(self, value, option_count):
+        return value.isdigit() and int(value) in range(1, option_count + 1)
